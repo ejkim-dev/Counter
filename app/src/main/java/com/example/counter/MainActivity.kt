469957e013a235802ac9cf4844fa3eb43784d8d3
ejkim <biz.ejkim@gmail.com>
2021-04-25 13:56:43 +0900

    DataBinding을 적용해서 뷰를 동작시킴

--- app/src/main/java/com/example/counter/MainActivity.kt
+++ app/src/main/java/com/example/counter/MainActivity.kt
@@ -1,77 +1,41 @@
 package com.example.counter
 
-import android.app.Activity
-import android.app.Application
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
+import androidx.databinding.DataBindingUtil
+import com.example.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<MainViewModel>()
 
+    /**data binding 으로 데이터들을 다 넘기면 되는데
+     * 그 데이터들이 사실상 viewmodel안에 다 들어있음
+     * 따라서 viewmodel을 통으로 넘기는게 가장 좋음*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         // 액티비티는 화면을 회전할 때마다 초기화가 된다. -> 문제 해결을 위해 viewmodel을 사용해야 함
         Log.d(TAG, "onCreate: ")
 
-        val text = findViewById<TextView>(R.id.counter_text)
+//        val text = findViewById<TextView>(R.id.counter_text)
+
+        binding.viewModel = viewModel
+        // 이 액티비티(or 프래그먼트)의 라이프사이클에 맞춰서 동작하게 설정
+        // 액티비티에서 동작하고, 뷰모델에서 값이 변경될 때마다 새로고침 하는 코드
+        binding.lifecycleOwner = this
 
         viewModel.apply {
             // count를 관찰해서 UI에 적용시킴 -> ui 갱신은 여기서 다 해결됨
-            countLiveData.observe(
+            /*countLiveData.observe(
                     this@MainActivity, Observer { count ->
                 text.text = "$count"
             }
-            )
+            )*/
 
-
-            findViewById<Button>(R.id.add_button).setOnClickListener {
-                increaseCount()
-            }
-            findViewById<Button>(R.id.sub_button).setOnClickListener {
-                decreaseCount()
-            }
         }
-
-
-        // 안드로이드 Q 부터 가능한 콜백함수
-        // kotlin에서는 object라는 키워드로 익명클래스를 구현하는데, 자바에서 new와 비슷한 역할을 함
-        registerActivityLifecycleCallbacks(object : Application.ActivityLifecycleCallbacks {
-            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                Log.d(TAG, "onActivityCreated: ")
-            }
-
-            override fun onActivityStarted(activity: Activity) {
-                Log.d(TAG, "onActivityStarted: ")
-            }
-
-            override fun onActivityResumed(activity: Activity) {
-                Log.d(TAG, "onActivityResumed: ")
-            }
-
-            override fun onActivityPaused(activity: Activity) {
-                Log.d(TAG, "onActivityPaused: ")
-            }
-
-            override fun onActivityStopped(activity: Activity) {
-                Log.d(TAG, "onActivityStopped: ")
-            }
-
-            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-                Log.d(TAG, "onActivitySaveInstanceState: ")
-            }
-
-            override fun onActivityDestroyed(activity: Activity) {
-                Log.d(TAG, "onActivityDestroyed: ")
-            }
-
-        })
 
     }
 
